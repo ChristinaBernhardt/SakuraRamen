ce59db5ef2aeb3f9a0add54697500da6e5fcf3ac
Christina Bernhardt <sozia76@web.de>
2023-12-29 23:20:30 +0100

    Outsourcing script

---
+++ sript.js
@@ -0,0 +1,43 @@
+function translateFunction1() {
+    document.getElementById('translate1').innerHTML = `DAS BESTE RAMEN DER STADT`;
+    document.getElementById('translate2').innerHTML = `BESTELLEN`;
+    document.getElementById('translate3').innerHTML = `UNSER MENÜ`;
+    document.getElementById('translate4').innerHTML = `Nudel`;
+    document.getElementById('translate5').innerHTML = `Brühe`;
+    document.getElementById('translate6').innerHTML = `Sonstiges`;
+    document.getElementById('translate7').innerHTML = `Menü`;
+    document.getElementById('translate8').innerHTML = `Adresse`;
+    document.getElementById('translate9').innerHTML = `Über uns`;
+    document.getElementById('translate10').innerHTML = `Wo Sie uns finden`;
+    document.getElementById('translate11').innerHTML = `HUHN`;
+    document.getElementById('translate12').innerHTML = `RIND`;
+    document.getElementById('translate13').innerHTML = `GARNELE`;
+    document.getElementById('translate14').innerHTML = `HUHN`;
+    document.getElementById('translate15').innerHTML = `RIND`;
+    document.getElementById('translate16').innerHTML = `GARNELE`;
+    document.getElementById('translate17').innerHTML = `HUHN`;
+    document.getElementById('translate18').innerHTML = `RIND`;
+    document.getElementById('translate19').innerHTML = `GARNELE`;
+
+  }
+  function translateFunction2() {
+    document.getElementById('translate1').innerHTML = `THE BEST RAMEN IN TOWN`;
+    document.getElementById('translate2').innerHTML = `HOW TO ORDER`;
+    document.getElementById('translate3').innerHTML = `OUR MENU`;
+    document.getElementById('translate4').innerHTML = `Pick your noodle`;
+    document.getElementById('translate5').innerHTML = `Pick your broth`;
+    document.getElementById('translate6').innerHTML = `Add your toppings`;
+    document.getElementById('translate7').innerHTML = `MENU`;
+    document.getElementById('translate8').innerHTML = `LOCATIONS`;
+    document.getElementById('translate9').innerHTML = `ABOUT`;
+    document.getElementById('translate10').innerHTML = `FIND US AT`;
+    document.getElementById('translate11').innerHTML = `CHICKEN`;
+    document.getElementById('translate12').innerHTML = `BEEF`;
+    document.getElementById('translate13').innerHTML = `SHRIMP`;
+    document.getElementById('translate14').innerHTML = `CHICKEN`;
+    document.getElementById('translate15').innerHTML = `BEEF`;
+    document.getElementById('translate16').innerHTML = `SHRIMP`;
+    document.getElementById('translate17').innerHTML = `CHICKEN`;
+    document.getElementById('translate18').innerHTML = `BEEF`;
+    document.getElementById('translate19').innerHTML = `SHRIMP`;
+  }
